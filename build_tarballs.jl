e22c8a5441ba2a82b91062ec6fad3fca2b39caf5
Elliot Saba <staticfloat@gmail.com>
2018-03-06 08:37:08 -0800

    Update to `build_tarballs()`

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,43 +1,41 @@
 using BinaryBuilder
 
-platforms = [
-  BinaryProvider.Windows(:i686),
-  BinaryProvider.Windows(:x86_64),
-  BinaryProvider.Linux(:i686, :glibc),
-  BinaryProvider.Linux(:x86_64, :glibc),
-  BinaryProvider.Linux(:aarch64, :glibc),
-  BinaryProvider.Linux(:armv7l, :glibc),
-  BinaryProvider.Linux(:powerpc64le, :glibc),
-  BinaryProvider.MacOS()
-]
-
+# Collection of sources required to build Nettle
 sources = [
   "https://ftp.gnu.org/gnu/nettle/nettle-3.3.tar.gz" =>
   "46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e",
 ]
 
+# Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/nettle-3.3/
 ./configure --prefix=$prefix --host=$target
-make -j3
+make -j${nproc}
 make install
 """
 
-products = prefix -> [
-  LibraryProduct(prefix,"libnettle"),
-  ExecutableProduct(prefix,"nettle-hash")
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+  Windows(:i686),
+  Windows(:x86_64),
+  Linux(:i686, :glibc),
+  Linux(:x86_64, :glibc),
+  Linux(:aarch64, :glibc),
+  Linux(:armv7l, :glibc),
+  Linux(:powerpc64le, :glibc),
+  MacOS()
 ]
 
-# Be quiet unless we've passed `--verbose`
-verbose = "--verbose" in ARGS
-ARGS = filter!(x -> x != "--verbose", ARGS)
+# The products that we will ensure are always built
+products(prefix) = [
+  LibraryProduct(prefix, "libnettle", :libnettle),
+  ExecutableProduct(prefix, "nettle-hash", :nettle_hash)
+]
 
-# Choose which platforms to build for; if we've got an argument use that one,
-# otherwise default to just building all of them!
-build_platforms = platforms
-if length(ARGS) > 0
-    build_platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(build_platforms), ", "))")
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
 
-autobuild(pwd(), "nettle", build_platforms, sources, script, products; verbose=verbose)
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "MbedTLS", sources, script, platforms, products, dependencies)

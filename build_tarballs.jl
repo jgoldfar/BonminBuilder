e7496ea94293115ab73d7d667b00c04ebfe0444e
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-22 01:00:13 -0700

    Update for versioned outputs

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,14 +1,16 @@
 using BinaryBuilder
 
 # Collection of sources required to build Nettle
+name = "Nettle"
+version = v"3.4"
 sources = [
-  "https://ftp.gnu.org/gnu/nettle/nettle-3.4.tar.gz" =>
-  "ae7a42df026550b85daca8389b6a60ba6313b0567f374392e54918588a411e94",
+    "https://ftp.gnu.org/gnu/nettle/nettle-3.4.tar.gz" =>
+    "ae7a42df026550b85daca8389b6a60ba6313b0567f374392e54918588a411e94",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/nettle-3.4/
+cd $WORKSPACE/srcdir/nettle-*/
 ./configure --prefix=$prefix --host=$target
 make -j${nproc}
 make install
@@ -17,21 +19,21 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-  Windows(:i686),
-  Windows(:x86_64),
-  Linux(:i686, :glibc),
-  Linux(:x86_64, :glibc),
-  Linux(:aarch64, :glibc),
-  Linux(:armv7l, :glibc),
-  Linux(:powerpc64le, :glibc),
-  MacOS(),
-  FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64),
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc),
+    Linux(:powerpc64le, :glibc),
+    MacOS(),
+    FreeBSD(:x86_64),
 ]
 
 # The products that we will ensure are always built
 products(prefix) = [
-  LibraryProduct(prefix, "libnettle", :libnettle),
-  ExecutableProduct(prefix, "nettle-hash", :nettle_hash)
+    LibraryProduct(prefix, "libnettle", :libnettle),
+    ExecutableProduct(prefix, "nettle-hash", :nettle_hash)
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -39,4 +41,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "Nettle", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
